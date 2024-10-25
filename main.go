7bb8fd9d7fb1fb65ee35776e82b810dbef29f384
Johan Grobler <johan@Quench.mobi>
2024-10-25 10:39:24 +0200

    Live

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Use(logger.New())
+	app.Use(cors.New())
+	//router.SetupRoutes(app)
+
+	app.Get("/live", func(c *fiber.Ctx) error {
+		return c.SendString("We are alive") // => 404 "Not Found"
+	})
+	// handle unavailable route
+	app.Use(func(c *fiber.Ctx) error {
+		return c.SendStatus(404) // => 404 "Not Found"
+	})
+	app.Listen(":8080")
+
+}
